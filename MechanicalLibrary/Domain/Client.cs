99f35e5afbfcc174ca01bc0485e68372053ab9d9
TITO\Tito <tito.badilla@hotmail.com>
2018-07-08 14:11:24 -0600

    Realización de proyecto libreria de clases y creacion de modelo dominio.

---
+++ MechanicalLibrary/Domain/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MechanicalLibrary.Domain
+{
+    public class Client
+    {
+        int idClient;
+        String clientName;
+        String lastName;
+        String phoneNumber;
+        String address;
+        String email;
+
+        public Client(int idClient, string clientName, string lastName, string phoneNumber, string address, string email)
+        {
+            this.IdClient = idClient;
+            this.ClientName = clientName;
+            this.LastName = lastName;
+            this.PhoneNumber = phoneNumber;
+            this.Address = address;
+            this.Email = email;
+        }
+
+        public Client()
+        {
+        }
+
+        public int IdClient { get => idClient; set => idClient = value; }
+        public string ClientName { get => clientName; set => clientName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public string Address { get => address; set => address = value; }
+        public string Email { get => email; set => email = value; }
+    }
+}
