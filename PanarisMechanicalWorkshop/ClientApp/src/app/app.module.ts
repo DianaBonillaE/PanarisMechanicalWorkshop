518eb182eb679b8302b1448afb308a0d705509d8
TITO\Tito <tito.badilla@hotmail.com>
2018-07-09 15:42:42 -0600

    Creacion de compontes report y product, ademas instalacion de librerias pdf y librerias para dropdown especial.

--- PanarisMechanicalWorkshop/ClientApp/src/app/app.module.ts
+++ PanarisMechanicalWorkshop/ClientApp/src/app/app.module.ts
@@ -10,25 +10,47 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
+//pdf import
+import { PDFExportModule } from '@progress/kendo-angular-pdf-export';
+
+
+//ng select
+import { NgSelectModule, NG_SELECT_DEFAULT_CONFIG } from '@ng-select/ng-select';
+import { ProductComponent } from './product/product.component';
+import { ReportComponent } from './report/report.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    ProductComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    PDFExportModule,
+    NgSelectModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'report', component: ReportComponent },
+      { path: 'product', component: ProductComponent }
+
     ])
   ],
-  providers: [],
+  providers: [{
+    provide: NG_SELECT_DEFAULT_CONFIG,
+    useValue: {
+        notFoundText: 'Este registro no es valido.'
+    }
+}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
