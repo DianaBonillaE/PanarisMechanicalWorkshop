85ed140c9a68f186ee0ef29fcac6c089295cdf4b
WIN-5SVJLAPR5BT\Tito <tito.badilla@hotmail.con>
2018-07-11 17:43:12 -0600

    services modificados y arreglos en product

--- Proyecto2DefinitivoLenguajes/Proyecto2Mechanical/ClientApp/src/app/product/product.component.ts
+++ Proyecto2DefinitivoLenguajes/Proyecto2Mechanical/ClientApp/src/app/product/product.component.ts
@@ -2,6 +2,10 @@ import { Component, NgModule, ViewChild } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormControl, FormGroup, ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { NgSelectModule, NgOption } from '@ng-select/ng-select';
+import { WorkDetail } from '../model/workDetail.model';
+import { WorkOrder } from '../model/workOrder.model';
+import { WorkOrderService } from '../service/work-order.service';
+import { WorkDetailOrderService } from '../service/work-detail-order.service';
 
 @Component({
   selector: 'app-product',
@@ -10,7 +14,11 @@ import { NgSelectModule, NgOption } from '@ng-select/ng-select';
 })
 export class ProductComponent {
 
-  
+  workDetail: WorkDetail[] = new Array();
+  workOrder: WorkOrder[] = new Array();
+  selectedWorkDetail: number;
+  selectedWorkOrder: number;
+
   pestanaSeleccionada: String = "uno";
   formularioInsertar: boolean = false;
   formularioEditar: boolean = false;
@@ -27,10 +35,30 @@ export class ProductComponent {
     { id: 1, name: 'ordenado' },
     { id: 0, name: 'No ordenado' }
   ];
+
   selectedCity: any;
-  constructor() { }
+
+  constructor(private workOrderService: WorkOrderService, private workDetailOrderService: WorkDetailOrderService) {
+    this.getAllOrders();
+  }
+  getAllOrders() {
+    this.workOrderService.getAllOrders().subscribe(data => this.workOrder = data);
+  }
+
+  getWorkDetailsByOrder(){
+    this.workDetailOrderService.getWorkDetailsByOrder(this.selectedWorkOrder).subscribe(
+      data=> this.workDetail=data
+    );
+  }
+
 
   ngOnInit() {
+  }
+
+  imprimir(){
+    console.log(this.workDetail.forEach(element => {
+      element
+    }));
   }
 
   cambiarPestana(value: String) {
